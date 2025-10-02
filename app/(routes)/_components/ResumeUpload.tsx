eab5706ccf5e895152e9c341b48ca38fe879c2b4
Subhadip Bera <subhodipbera10@gmail.com>
2025-10-02 20:12:06 +0530

    add n8n

--- app/(routes)/_components/ResumeUpload.tsx
+++ app/(routes)/_components/ResumeUpload.tsx
@@ -12,7 +12,8 @@ export function ResumeUpload() {
   return (
     <div className='flex items-center justify-center'>
     <div className="w-full flex items-center justify-center min-h-96">
-      <div className="w-full max-w-2xl min-h-60 flex items-center justify-center border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg   rounded-2xl">
+      <div className="w-full max-w-2xl min-h-60 flex items-center justify-center border border-dashed bg-white dark:bg-black border-neutral-200
+       dark:border-neutral-800 rounded-lg   rounded-2xl">
         <FileUpload onChange={handleFileUpload} />
       </div>
     </div>
